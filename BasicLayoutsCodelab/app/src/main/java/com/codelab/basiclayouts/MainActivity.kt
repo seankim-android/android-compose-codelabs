13bc00bef12dd9a2685ebdb17d8715d26278614c
Sean Kim <kssc0112@gmail.com>
2022-09-17 21:59:10 -0700

    Step: Search bar - Modifiers

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -21,9 +21,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
@@ -35,12 +44,25 @@ class MainActivity : ComponentActivity() {
   }
 }
 
-// Step: Search bar - Modifiers
 @Composable
 fun SearchBar(
   modifier: Modifier = Modifier
 ) {
-  // Implement composable here
+  TextField(
+    value = "",
+    onValueChange = {},
+    modifier = modifier
+      .fillMaxWidth()
+      .heightIn(min = 56.dp),
+    placeholder = { stringResource(id = R.string.placeholder_search) },
+    colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
+    leadingIcon = {
+      Icon(
+        imageVector = Icons.Default.Search,
+        contentDescription = null
+      )
+    }
+  )
 }
 
 // Step: Align your body - Alignment
@@ -98,7 +120,7 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
 // Step: MySoothe App - Scaffold
 @Composable
 fun MySootheApp() {
-  // Implement composable here
+  SearchBar(Modifier.padding(8.dp))
 }
 
 private val alignYourBodyData = listOf(
