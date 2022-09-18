a4f4dac827939fada23b100e1ff8400be8c16f43
Sean Kim <kssc0112@gmail.com>
2022-09-18 11:58:53 -0700

    Favorite collections grid - LazyGrid

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -24,6 +24,9 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
@@ -140,12 +143,25 @@ fun AlignYourBodyRow(
   }
 }
 
-// Step: Favorite collections grid - LazyGrid
 @Composable
 fun FavoriteCollectionsGrid(
   modifier: Modifier = Modifier
 ) {
-  // Implement composable here
+  LazyHorizontalGrid(
+    rows = GridCells.Fixed(2),
+    modifier = modifier.height(120.dp),
+    contentPadding = PaddingValues(16.dp),
+    horizontalArrangement = Arrangement.spacedBy(8.dp),
+    verticalArrangement = Arrangement.spacedBy(8.dp)
+  ) {
+    items(favoriteCollectionsData) { item ->
+      FavoriteCollectionCard(
+        item.drawable,
+        item.text,
+        modifier.height(56.dp)
+      )
+    }
+  }
 }
 
 // Step: Home section - Slot APIs
